6c1acec6d78e3ec7039bef205d736bb5867a1006
guillermo-delgado-revature <guillermo.delgado@revature.net>
2021-06-21 12:40:34 -0400

    W4 hackerChalleng 'Stair'

---
+++ W4KackerRankGuillermoDelgado/Stair/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Stair
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             int n = 6;
+    
+         for(int i = n-1; i >= 0; i--){  
+             System.Console.WriteLine("");
+            for(int j = 0; j < n ; j++){
+
+                if(i == j || j > i){
+
+                    Console.Write("#");                    
+                }else{
+                    System.Console.Write(" ");
+                }
+            }
+
+            
+         }
+        
+        }
+    }    
+}
